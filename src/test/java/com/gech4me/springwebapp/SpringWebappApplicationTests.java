c6cbc4198505c0d2fde91b211a2be05e7506e76e
Getachew Mulat <gech4me@gmail.com>
2020-02-11 11:14:52 +0300

    first comment

---
+++ src/test/java/com/gech4me/springwebapp/SpringWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gech4me.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
